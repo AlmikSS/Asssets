8e558326bbc2d61a43266bdedd2f892ee3486bd3
AlmikSS <ulmasoviskandar7@gmail.com>
2026-08-29 04:19:23 +0300

    1

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\UnityProjects\\KofeyekToolkit\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\UnityProjects\\KofeyekToolkit\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\UnityProjects\\KofeyekToolkit\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\UnityProjects\\KofeyekToolkit\\website\\src\\css\\custom.css"),
+];
